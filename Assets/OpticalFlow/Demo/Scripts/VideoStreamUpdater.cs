13921df608fcd545d3ea94a2c8693a49f80ea99c
NoiseCrime <noisecrime@users.noreply.github.com>
2023-02-13 19:23:26 +0000

    Added support for VideoPlayer
    Added support for Unity VideoPlayer.
    Version updated to 2017.4.23f1 - as its what I have installed but don't think it matters.
    In testing though 2017.4 does not seem to properly support many file formats via URL. Update to 2018.4 to get better video format support in Unity.

---
+++ Assets/OpticalFlow/Demo/Scripts/VideoStreamUpdater.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace OpticalFlow.Demo
+{
+
+    public class VideoStreamUpdater : TextureUpdater {
+
+        [SerializeField] protected VideoPlayer videoPlayer;
+
+        void Start () {
+            videoPlayer.Play();
+        }
+
+        void Update ()
+        {
+            videoPlayer.Pause();
+            
+            if (videoPlayer.frame / videoPlayer.frameRate < Time.time) 
+            {
+                videoPlayer.StepForward();
+
+                if ( null != videoPlayer.texture)
+                    textureUpdateEvent.Invoke(videoPlayer.texture);
+            }
+        }
+        /*
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(webCamTexture, destination);
+        }
+        */
+        protected void OnDestroy()
+        {
+        }
+
+    }
+
+}
+
+
